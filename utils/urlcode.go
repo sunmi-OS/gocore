9ccf0b2a5339683d2198bdd9a5336810454b9446
wenzhenxi <wenzhenxi@sunmi.com>
2018-05-15 11:27:12 +0800

    update rulcode

---
+++ utils/urlcode.go
@@ -0,0 +1,27 @@
+package utils
+
+import "net/url"
+
+// URL编码
+func UrlEncode(urls string) (string, error) {
+
+	//UrlEnCode编码
+	urlStr, err := url.Parse(urls)
+	if err != nil {
+		return "", err
+	}
+
+	return urlStr.RequestURI(), nil
+}
+
+// URL解码
+func UrlDecode(urls string) (string, error) {
+
+	//UrlEnCode解码
+	urlStr, err := url.Parse(urls)
+	if err != nil {
+		return "", err
+	}
+
+	return urlStr.Path, nil
+}
