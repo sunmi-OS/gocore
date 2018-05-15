18ecd47a9aafd4d9b5eec02d3c7fd1826d875345
lisensen <lisensen@sunmi.com>
2018-05-15 15:55:29 +0800

    change page of aes

--- gorm/gorm.go
+++ gorm/gorm.go
@@ -8,6 +8,9 @@ import (
 	"github.com/jinzhu/gorm"
 	"github.com/sunmi-OS/gocore/viper"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 var Gorm sync.Map
