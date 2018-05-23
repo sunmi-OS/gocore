6bb12d4a08b7ee8fe288c1041ed35cd030b60fb0
wenzhenxi <wenzhenxi@sunmi.com>
2018-05-23 09:41:17 +0800

    update log fielupdate

--- log/log.go
+++ log/log.go
@@ -11,7 +11,6 @@ import (
 )
 
 var Logger *zap.Logger
-var day string
 var logfile *os.File
 var isDebug bool
 var cfg zap.Config
@@ -50,29 +49,35 @@ func InitLogger(serviceaName string, debug bool) {
 	}
 	Logger.Info("logger初始化成功")
 	isDebug = debug
-	day = time.Now().Format("02")
-
+	go updateLogFile()
 }
 
 // 检测是否跨天了,把记录记录到新的文件目录中
 func updateLogFile() {
 	var err error
-	day2 := time.Now().Format("02")
-	if day2 != day {
-		logfile.Close()
-		filename := utils.GetPath() + "/Runtime/" + time.Now().Format("2006-01-02") + ".log"
-		logfile, err = os.Create(utils.GetPath() + "/Runtime/" + time.Now().Format("2006-01-02") + ".log")
-		if err != nil {
-			fmt.Println(err)
+	for {
+		now := time.Now()
+		// 计算下一个零点
+		next := now.Add(time.Hour * 24)
+		next = time.Date(next.Year(), next.Month(), next.Day(), 0, 0, 0, 0, next.Location())
+		t := time.NewTimer(next.Sub(now))
+		select {
+		case <-t.C:
+			//以下为定时执行的操作
+			logfile.Close()
+			filename := utils.GetPath() + "/Runtime/" + time.Now().Format("2006-01-02") + ".log"
+			logfile, err = os.Create(utils.GetPath() + "/Runtime/" + time.Now().Format("2006-01-02") + ".log")
+			if err != nil {
+				fmt.Println(err)
+			}
+			cfg.ErrorOutputPaths = []string{filename}
+			cfg.OutputPaths = []string{filename}
+			Logger, err = cfg.Build()
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
 		}
-		cfg.ErrorOutputPaths = []string{filename}
-		cfg.OutputPaths = []string{filename}
-		Logger, err = cfg.Build()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		day = day2
 	}
 }
 
@@ -81,18 +86,15 @@ func LogDebug(msg string, fields ...zap.Field) {
 	if isDebug == false {
 		return
 	}
-	updateLogFile()
 	Logger.Debug(msg, fields...)
 }
 
 // 记录Info信息
 func LogInfo(msg string, fields ...zap.Field) {
-	updateLogFile()
 	Logger.Info(msg, fields...)
 }
 
 // 记录Error信息
 func LogError(msg string, fields ...zapcore.Field) {
-	updateLogFile()
 	Logger.Error(msg, fields...)
 }
