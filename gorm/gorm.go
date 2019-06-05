709d497bbc5add0a62301549cc18380f30511537
wenzhenxi <wenzhenxi@sunmi.com>
2019-06-05 16:57:03 +0800

    update gorm error

--- gorm/gorm.go
+++ gorm/gorm.go
@@ -1,6 +1,7 @@
 package gorm
 
 import (
+	"errors"
 	"fmt"
 	"sync"
 	"time"
@@ -12,6 +13,19 @@ import (
 )
 
 var Gorm sync.Map
+
+var (
+	// ErrRecordNotFound record not found error, happens when haven't find any matched data when looking up with a struct
+	ErrRecordNotFound = errors.New("record not found")
+	// ErrInvalidSQL invalid SQL error, happens when you passed invalid SQL
+	ErrInvalidSQL = errors.New("invalid SQL")
+	// ErrInvalidTransaction invalid transaction when you are trying to `Commit` or `Rollback`
+	ErrInvalidTransaction = errors.New("no valid transaction")
+	// ErrCantStartTransaction can't start transaction when you are trying to start one with `Begin`
+	ErrCantStartTransaction = errors.New("can't start transaction")
+	// ErrUnaddressable unaddressable value
+	ErrUnaddressable = errors.New("using unaddressable value")
+)
 
 // 初始化Gorm
 func NewDB(dbname string) {
