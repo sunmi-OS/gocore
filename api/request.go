7343d90c2180cc4a4838cd05e1f5197ef5b9b666
icechen <sunbo@sunmi.com>
2019-07-12 11:15:08 +0800

    decode失败不直接返回error，避免未encode的参数含有"%"，导致的失败

--- api/request.go
+++ api/request.go
@@ -139,10 +139,10 @@ func (this *Request) InitDES() error {
 			//如果是加密的params那么进行解密操作
 			if isEncrypted == "1" {
 				// webapi 情况下需要对params decode
-				var err error
-				params, err = url.PathUnescape(params)
-				if err != nil {
-					return err
+				// decode 失败，可能未 encode，直接使用原 params 解密
+				decodeParams, err := url.PathUnescape(params)
+				if err == nil {
+					params = decodeParams
 				}
 				base64params, err := base64.StdEncoding.DecodeString(params)
 				if err != nil {
