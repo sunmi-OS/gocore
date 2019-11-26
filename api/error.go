3c66abe64a84530face063404900006c92e4703f
wenzhenxi <wenzhenxi@vip.qq.com>
2019-11-26 16:17:39 +0800

    update

--- api/error.go
+++ api/error.go
@@ -3,8 +3,7 @@ package api
 import (
 	"github.com/labstack/echo"
 	"github.com/spf13/cast"
-	"github.com/sunmi-OS/gocore/log"
-	"go.uber.org/zap"
+	"log"
 )
 
 // 捕获panic异样防止程序终止 并且记录到日志
@@ -12,7 +11,7 @@ func ErrorLogRecover(c echo.Context) {
 
 	if err := recover(); err != nil {
 		c.Response().Write([]byte("系统错误!具体原因:" + cast.ToString(err)))
-		log.Sugar.Errorw("example-log:err", zap.Error(err.(error)), map[string]interface{}{
+		log.Println("example-log:err", err.(error), map[string]interface{}{
 			"URL.Path":    c.Request().URL.Path,
 			"QueryParams": c.QueryParams(),
 		})
