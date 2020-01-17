95e8eaea1b559a324b3661787c3bff7bbffc815c
wenzhenxi <wenzhenxi@sunmi.com>
2020-01-17 18:17:45 +0800

    update nacos toml check toml Merage

--- main.go
+++ main.go
@@ -1,12 +1,62 @@
 package main
 
+import "C"
 import (
+	"bytes"
 	"fmt"
+	"github.com/spf13/viper"
+	"github.com/BurntSushi/toml"
+	"log"
 )
+
+
+
+var str = `
+[server]
+mode = "development"
+port = 8080
+debug = true
+http_timeout = 20000
+jwt_timeout = 8640000
+log_path = "./log/tix-log.log"
+maps = "./map_data.json"
+order_timeout = 600
+
+[development]
+baseURL = "http://dev.xxxxx.com:2121/partner-service/"
+clientID = "xxxx"
+schedule_expired_before = 900
+promo_activity_id = "ttttttt"
+
+[production]
+baseURL = "http://prod.xxxxx.com:2121/partner-service/"
+clientID = "yyyy"
+schedule_expired_before = 900
+promo_activity_id = "kkkkkkkkkk"`
 
 func main() {
 
 	fmt.Println("gocore")
+
+	V := viper.New()
+	V.SetConfigType("toml")
+	err := V.MergeConfig(bytes.NewBuffer([]byte(str)))
+	if err != nil {
+		print(err)
+	}
+
+
+
+	var tmp interface{}
+
+	if _, err := toml.Decode(str, &tmp); err != nil {
+
+		log.Fatalf("Error decoding TOML: %s", err)
+	}
+
+	s := V.GetString("server.mode")
+
+	fmt.Println(s)
 
 	// use example
 	//hook.ShowExample()
