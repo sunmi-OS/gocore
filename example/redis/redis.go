d99a0f54c52f31425388970a24958d475bf361e1
liuguoqiang <liuguoqiang@xiaoheiban.cn>
2020-03-27 17:58:07 +0800

    fix:redis

--- example/redis/redis.go
+++ example/redis/redis.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
-	"github.com/sunmi-OS/gocore/redis"
+
+	"gocore/redis"
+
 	"github.com/sunmi-OS/gocore/viper"
 )
 
 func main() {
 	viper.NewConfig("config", "conf")
+	redis.GetRedisOptions("e_invoice")
+	redis.GetRedisDB("e_invoice").Set("test", "sunmi", 0)
+	fmt.Println(redis.GetRedisDB("e_invoice").Get("test").String())
 
-	redis.GetRedisOptions("email_check")
-	redis.GetRedisDB("email_check").Set("test", "sunmi")
-	fmt.Println(redis.GetRedisDB("encryption").Get("test").String())
+	redis.GetRedisOptions("OtherRedisServer.e_invoice")
+	redis.GetRedisDB("OtherRedisServer.e_invoice").Set("test", "sunmi_other", 0)
+	fmt.Println(redis.GetRedisDB("OtherRedisServer.e_invoice").Get("test").String())
+	fmt.Println(redis.GetRedisDB("e_invoice").Get("test").String())
 }
