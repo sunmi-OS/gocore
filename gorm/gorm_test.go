eba105179e20658ab29d72f077180b9f90704eb7
Jerry <85411418@qq.com>
2020-09-09 16:49:59 +0800

    rebuild gorm (#53)

---
+++ gorm/gorm_test.go
@@ -0,0 +1,16 @@
+package gorm
+
+import (
+	"testing"
+)
+
+func TestNewDB(t *testing.T) {
+	//NewDB("a")
+	//NewDB("b")
+	//NewDB("c")
+
+	// only example
+	//Gorm().GetORM().Select("id").Where("id = ?", 1).First(nil)
+
+	//Gorm().Close()
+}
