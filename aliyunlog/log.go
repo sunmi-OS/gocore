79910ae147ef1b823b6a525b4e3e4d873dec228d
喵了个咪 <wenzhenxi@vip.qq.com>
2020-11-26 11:31:17 +0800

    Update log.go
    feat: update aliyunlog add auto create LogStore index

--- aliyunlog/log.go
+++ aliyunlog/log.go
@@ -2,13 +2,17 @@ package aliyunlog
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"time"
 
+	sls "github.com/aliyun/aliyun-log-go-sdk"
 	"github.com/aliyun/aliyun-log-go-sdk/producer"
 	"github.com/sunmi-OS/gocore/viper"
+	"github.com/tidwall/gjson"
 )
 
+// AliyunLog 阿里云日志配置结构体
 type AliyunLog struct {
 	AccessKey string
 	SecretKey string
@@ -19,8 +23,10 @@ type AliyunLog struct {
 	Log       *producer.Producer
 }
 
+// LogClient 对外原生实例
 var LogClient AliyunLog
 
+// InitLog 初始化日志
 func InitLog(configName, LogStore string) {
 	hostname, _ := os.Hostname()
 	LogClient = AliyunLog{
@@ -35,6 +41,8 @@ func InitLog(configName, LogStore string) {
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println("test")
 
 	producerConfig := producer.GetDefaultProducerConfig()
 	producerConfig.Endpoint = LogClient.Endpoint
@@ -51,25 +59,68 @@ func InitLog(configName, LogStore string) {
 	}
 }
 
+// Info 记录info日志
 func Info(topic string, logs map[string]string) error {
 	logs["level"] = "info"
 	logmsg := producer.GenerateLog(uint32(time.Now().Unix()), logs)
 	return LogClient.Log.SendLog(LogClient.Project, LogClient.LogStore, topic, LogClient.HostName, logmsg)
 }
 
+// Error 记录异常日志
 func Error(topic string, logs map[string]string) error {
 	logs["level"] = "error"
 	logmsg := producer.GenerateLog(uint32(time.Now().Unix()), logs)
 	return LogClient.Log.SendLog(LogClient.Project, LogClient.LogStore, topic, LogClient.HostName, logmsg)
 }
 
+// Close 关闭日志服务
 func Close() {
-	LogClient.Log.SafeClose()
+	if LogClient.Log != nil {
+		LogClient.Log.SafeClose()
+	}
 }
 
+// checkConfig 验证配置是否缺少 自动创建LogStore
 func checkConfig(conf AliyunLog) (err error) {
 	if conf.AccessKey == "" || conf.Endpoint == "" || conf.Project == "" || conf.LogStore == "" || conf.SecretKey == "" {
 		err = errors.New("config Missing parameter")
 	}
+
+	// 创建 LogStore 默认存储30天，2个分片自动扩容最大64片
+	Client := sls.CreateNormalInterface(conf.Endpoint, conf.AccessKey, conf.SecretKey, "")
+	err = Client.CreateLogStore(conf.Project, conf.LogStore, 30, 2, true, 64)
+	if err != nil {
+		if gjson.Parse(err.Error()).Get("errorCode").String() == "LogStoreAlreadyExist" {
+			err = nil
+		}
+	}
+
+	// 创建索引
+	index := sls.Index{
+		Keys: map[string]sls.IndexKey{
+			"content": {
+				Token:         []string{`,`, ` `, `'`, `"`, `;`, `=`, `(`, `)`, `[`, `]`, `{`, `}`, `?`, `@`, `&`, `<`, `>`, `/`, `:`, `\n`, `\t`, `\r`},
+				CaseSensitive: false,
+				Type:          "text",
+				Chn:           true,
+				DocValue:      true,
+			},
+		},
+		Line: &sls.IndexLine{
+			Token:         []string{`,`, ` `, `'`, `"`, `;`, `=`, `(`, `)`, `[`, `]`, `{`, `}`, `?`, `@`, `&`, `<`, `>`, `/`, `:`, `\n`, `\t`, `\r`},
+			CaseSensitive: false,
+			IncludeKeys:   []string{},
+			ExcludeKeys:   []string{},
+			Chn:           true,
+		},
+	}
+	err = Client.CreateIndex(conf.Project, conf.LogStore, index)
+	if err != nil {
+		if gjson.Parse(err.Error()).Get("errorCode").String() == "IndexAlreadyExist" {
+			err = nil
+		}
+	}
+
+	Client.Close()
 	return
 }
