d3536543a8bf88ab340d760954e428a5316a1ec2
Jerry <85411418@qq.com>
2021-07-28 09:55:05 +0800

    delete some pkg

--- api/resutful/web/model.go
+++ api/resutful/web/model.go
@@ -1,21 +1,10 @@
 package web
 
-import (
-	"github.com/sunmi-OS/gocore/v2/utils/limit"
-	"github.com/sunmi-OS/gocore/v2/utils/trace"
-)
-
 type Config struct {
 	// http host
 	Host string
 	// http export port  :8080
 	Port string
-
-	// interface limit
-	Limit *limit.Config
-
-	// jaeger trace config
-	Trace *trace.Config
 }
 
 type RecoverInfo struct {
