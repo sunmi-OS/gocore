d6eaf689679aa6d0e2259ad06d95814edfa11e99
wenzhenxi <wenzhenxi@vip.qq.com>
2021-08-10 15:47:53 +0800

    feat: up

--- conf/nacos/viper.go
+++ conf/nacos/viper.go
@@ -4,9 +4,9 @@ import (
 	"errors"
 
 	"github.com/BurntSushi/toml"
-	"github.com/sunmi-OS/gocore/v2/conf/viper"
-
 	"github.com/nacos-group/nacos-sdk-go/vo"
+	"github.com/sunmi-OS/gocore/v2/conf/viper"
+	"github.com/sunmi-OS/gocore/v2/glog"
 )
 
 type configParam struct {
@@ -24,6 +24,20 @@ func GetViper() *ViperToml {
 	return nacosHarder.vt
 }
 
+// NacosToViper 同步Nacos读取的配置注入Viper
+func (vt *ViperToml) NacosToViper() {
+	s, err := vt.GetConfig()
+	if err != nil {
+		panic(err)
+	}
+	viper.MergeConfigToToml(s)
+}
+
+// SetBaseConfig 注入基础配置
+func (vt *ViperToml) SetBaseConfig(configs string) {
+	viper.MergeConfigToToml(configs)
+}
+
 // GetConfig 获取整套配置文件
 func (vt *ViperToml) GetConfig() (string, error) {
 	if nacosHarder.local {
@@ -34,27 +48,22 @@ func (vt *ViperToml) GetConfig() (string, error) {
 		return configs, nil
 	}
 	var configs = ""
-	for _, dataIdorGroup := range vt.dataIdOrGroupList {
-		group := dataIdorGroup.group
-		for _, v := range dataIdorGroup.dataIds {
-			content, err := nacosHarder.icc.GetConfig(vo.ConfigParam{
-				DataId: v,
-				Group:  group})
+	for _, dataIdOrGroup := range vt.dataIdOrGroupList {
+		group := dataIdOrGroup.group
+		for _, v := range dataIdOrGroup.dataIds {
+			content, err := nacosHarder.icc.GetConfig(vo.ConfigParam{DataId: v, Group: group})
 			if err != nil {
 				return "", err
 			}
 			configs += "\r\n" + content
-			if len(vt.callbackList) > 0 {
-				// 注册回调
-				grouptodataId := group + v
-				err := nacosHarder.icc.ListenConfig(vo.ConfigParam{
-					DataId:   v,
-					Group:    group,
-					OnChange: vt.callbackList[grouptodataId],
-				})
-				if err != nil {
-					return "", err
-				}
+			// 注册回调
+			err = nacosHarder.icc.ListenConfig(vo.ConfigParam{
+				DataId:   v,
+				Group:    group,
+				OnChange: vt.callbackList[group+v],
+			})
+			if err != nil {
+				glog.Error(group+"\r\n"+v+"\r\n ListenConfig Error", err)
 			}
 		}
 	}
@@ -75,35 +84,21 @@ func (vt *ViperToml) GetConfigParse(confPtr interface{}) error {
 }
 
 // SetDataIds 设置需要读取哪些配置
+// 配置默认回调方法更新配置
 func (vt *ViperToml) SetDataIds(group string, dataIds ...string) {
 	vt.dataIdOrGroupList = append(vt.dataIdOrGroupList, configParam{group: group, dataIds: dataIds})
 	for _, v := range dataIds {
-		groupToDataId := group + v
-		vt.callbackList[groupToDataId] = func(namespace, group, dataId, data string) {
-			panic(errors.New(namespace + "\r\n" + group + "\r\n" + dataId + "\r\n" + data + "\r\n Updata Config"))
+		vt.callbackList[group+v] = func(namespace, group, dataId, data string) {
+			vt.NacosToViper()
+			glog.Error(errors.New(namespace + "\r\n" + group + "\r\n" + dataId + "\r\n" + data + "\r\n Update Config"))
 		}
 	}
 }
 
-// SetCallBackFunc 配置回调方法
+// SetCallBackFunc 配置自定义回调方法
 func (vt *ViperToml) SetCallBackFunc(group, dataId string, callback func(namespace, group, dataId, data string)) {
-	groupToDataId := group + dataId
-	vt.callbackList[groupToDataId] = func(namespace, group, dataId, data string) {
+	vt.callbackList[group+dataId] = func(namespace, group, dataId, data string) {
 		vt.NacosToViper()
 		callback(namespace, group, dataId, data)
 	}
-}
-
-// NacosToViper 同步Nacos读取的配置注入Viper
-func (vt *ViperToml) NacosToViper() {
-	s, err := vt.GetConfig()
-	if err != nil {
-		panic(err)
-	}
-	viper.MergeConfigToToml(s)
-}
-
-// SetViperBase 注入基础配置
-func (vt *ViperToml) SetViperBase(configs string) {
-	viper.MergeConfigToToml(configs)
 }
