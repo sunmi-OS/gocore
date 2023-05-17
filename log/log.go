996c9f8e72deca553f7d8188484f7da2cc1b8547
liuning <liuning@sunmi.com>
2023-05-17 18:21:28 +0800

    update log tool

--- log/log.go
+++ log/log.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 
 	"github.com/sunmi-OS/gocore/utils"
@@ -17,44 +18,84 @@ var (
 	Sugar   *zap.SugaredLogger
 	logfile *os.File
 	cfg     zap.Config
+	once    sync.Once
 )
 
-// 初始化Log日志记录
-func InitLogger(serviceName string) {
+type Option func(*zap.Config)
+
+type OutPut int
+
+const (
+	ConsoleOutPut OutPut = iota + 1
+	FileOutPut
+	ConsoleAndFileOutPut
+)
+
+// InitLogger 初始化Log日志记录
+func InitLogger(serviceName string, opt ...Option) {
 	var err error
-
-	if !utils.IsDirExists(utils.GetPath() + "/Runtime") {
-		if mkdirerr := utils.MkdirFile(utils.GetPath() + "/Runtime"); mkdirerr != nil {
-			fmt.Println(mkdirerr)
-		}
-	}
-
-	filename := utils.GetPath() + "/Runtime/" + time.Now().Format("2006-01-02") + ".log"
-	logfile, err = os.OpenFile(filename, os.O_RDWR|os.O_APPEND, 0666)
-	if err != nil {
-		logfile, err = os.Create(filename)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-
 	cfg = zap.NewProductionConfig()
-	cfg.OutputPaths = []string{filename, "stdout"}
-	cfg.ErrorOutputPaths = []string{filename, "stderr"}
+	cfg.OutputPaths = []string{"stdout"}
+	cfg.ErrorOutputPaths = []string{"stderr"}
 	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-
-	viper.C.SetDefault("system.debug", "true")
-	if viper.GetEnvConfigBool("system.debug") {
-		cfg.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
-	}
 	Logger, err = cfg.Build()
-
 	if err != nil {
-		fmt.Println(err)
+		panic("init logger error")
 	}
 	Sugar = Logger.Sugar()
+	for _, option := range opt {
+		option(&cfg)
+	}
+}
 
-	go updateLogFile()
+// SetOutputPath Set log output Path
+func SetOutputPath(outputPath OutPut) Option {
+	return func(c *zap.Config) {
+		if outputPath == FileOutPut || outputPath == ConsoleAndFileOutPut {
+			var err error
+			if !utils.IsDirExists(utils.GetPath() + "/Runtime") {
+				if mkdirerr := utils.MkdirFile(utils.GetPath() + "/Runtime"); mkdirerr != nil {
+					panic(mkdirerr)
+				}
+			}
+			filename := utils.GetPath() + "/Runtime/" + time.Now().Format("2006-01-02") + ".log"
+			logfile, err = os.OpenFile(filename, os.O_RDWR|os.O_APPEND, 0666)
+			if err != nil {
+				logfile, err = os.Create(filename)
+				if err != nil {
+					panic(err)
+				}
+			}
+			if outputPath == FileOutPut {
+				c.OutputPaths = []string{filename}
+				c.ErrorOutputPaths = []string{filename}
+			} else {
+				c.OutputPaths = append(c.OutputPaths, filename)
+				c.ErrorOutputPaths = append(c.ErrorOutputPaths, filename)
+			}
+			Logger, err = c.Build()
+			if err != nil {
+				panic("init logger error")
+			}
+			Sugar = Logger.Sugar()
+			once.Do(func() {
+				go updateLogFile()
+			})
+		}
+	}
+}
+
+// SetLogLevel alters the logging level.
+func SetLogLevel(l zapcore.Level) Option {
+	return func(c *zap.Config) {
+		var err error
+		c.Level = zap.NewAtomicLevelAt(l)
+		Logger, err = c.Build()
+		if err != nil {
+			panic("init logger error")
+		}
+		Sugar = Logger.Sugar()
+	}
 }
 
 // 检测是否跨天了,把记录记录到新的文件目录中
@@ -71,16 +112,28 @@ func updateLogFile() {
 		t := time.NewTimer(next.Sub(now))
 		select {
 		case <-t.C:
-			//以下为定时执行的操作
+			// 以下为定时执行的操作
 			logfile.Close()
-			go deleteLog(logPath, saveDays) //删除修改时间在saveDays天前的文件
+			go deleteLog(logPath, saveDays) // 删除修改时间在saveDays天前的文件
 			filename := logPath + time.Now().Format("2006-01-02") + ".log"
 			logfile, err = os.Create(logPath + time.Now().Format("2006-01-02") + ".log")
 			if err != nil {
 				fmt.Println(err)
 			}
-			cfg.ErrorOutputPaths = []string{filename, "stderr"}
-			cfg.OutputPaths = []string{filename, "stdout"}
+			var outPaths []string
+			var errPaths []string
+			for _, v := range cfg.OutputPaths {
+				if v == "stdout" {
+					// cfg.ErrorOutputPaths = []string{filename, "stderr"}
+					outPaths = append(outPaths, v)
+					errPaths = append(errPaths, "stderr")
+				} else {
+					outPaths = append(outPaths, filename)
+					errPaths = append(errPaths, filename)
+				}
+			}
+			cfg.OutputPaths = outPaths
+			cfg.ErrorOutputPaths = errPaths
 			Logger, err = cfg.Build()
 			if err != nil {
 				fmt.Println(err)
