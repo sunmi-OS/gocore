15581efb4015eda99dd99286e184b8c397aec670
ming.guo <ming.guo@sunmi.com>
2023-06-15 17:45:33 +0800

    fix: remove prefix method

--- mq/gokafka.v2/consumer_test.go
+++ mq/gokafka.v2/consumer_test.go
@@ -2,7 +2,6 @@ package gokafka
 
 import (
 	"context"
-	"os"
 	"testing"
 	"time"
 
@@ -11,42 +10,6 @@ import (
 
 	"github.com/segmentio/kafka-go"
 )
-
-func TestPrefix(t *testing.T) {
-	envstr := os.Getenv("RUN_TIME")
-	os.Setenv("RUN_TIME", "dev")
-
-	testCases := []struct {
-		input    string
-		expected string
-	}{
-		{"Hello", "Dev-Hello"},
-		{"", "Dev-"},
-		{"this is a long string", "Dev-this is a long string"},
-	}
-
-	for _, tc := range testCases {
-		result := RunTimePrefix(tc.input)
-		if result != tc.expected {
-			t.Errorf("Unexpected result for input %v: got %v, want %v", tc.input, result, tc.expected)
-		}
-
-		result = PreTopicPrefix(tc.input)
-		if result != tc.expected {
-			t.Errorf("Unexpected result for input %v: got %v, want %v", tc.input, result, tc.expected)
-		}
-	}
-	err := os.Setenv("RUN_TIME", "pre")
-	if err != nil {
-		t.Errorf("set RUN_TIME failed err=%v", err)
-	}
-	result := PreTopicPrefix("hello")
-	if result != "Onl-hello" {
-		t.Errorf("Unexpected result for input %v: got %v, want %v", "hello", result, "Onl-hello")
-	}
-
-	os.Setenv("RUN_TIME", envstr)
-}
 
 func TestConsumer(t *testing.T) {
 	brokers := []string{}                            // TODO: add your brokers
