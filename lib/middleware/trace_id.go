2648ed8ae484a28f9155a32deab913e6ee4a86ac
shenghui0779 <shenghui0779@gmail.com>
2024-11-27 09:39:40 +0800

    Update TraceId

--- lib/middleware/trace_id.go
+++ lib/middleware/trace_id.go
@@ -2,32 +2,32 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/sunmi-OS/gocore/v2/utils"
 	"google.golang.org/grpc/metadata"
 )
 
 // TraceId is a middleware that injects a trace ID into the context of each request.
-func TraceId(traceKey string, traceHeaders ...string) gin.HandlerFunc {
+func TraceId() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		md, ok := metadata.FromIncomingContext(c.Request.Context())
 		if !ok {
 			md = metadata.Pairs()
 		}
 		// traceId已存在，则复用
-		if len(md.Get(traceKey)) != 0 {
+		if len(md.Get(utils.XB3TraceId)) != 0 {
 			c.Next()
 			return
 		}
-		// 取traceId，优先级按照headers顺序
-		// 如：[x-b3-traceid,x-request-id]，若x-b3-traceid取不到，则取x-request-id
-		for _, h := range traceHeaders {
-			if v := c.GetHeader(h); len(v) != 0 {
-				// 设置traceId
-				md.Set(traceKey, v)
-				ctx := metadata.NewIncomingContext(c.Request.Context(), md)
-				c.Request = c.Request.WithContext(ctx)
-				break
-			}
+		// 去header取traceId
+		traceId := c.GetHeader(utils.XB3TraceId)
+		// 找不到x-b3-traceid，用x-request-id
+		if len(traceId) == 0 {
+			traceId = c.GetHeader(utils.XRequestId)
 		}
+		// 设置traceId
+		md.Set(utils.XB3TraceId, traceId)
+		ctx := metadata.NewIncomingContext(c.Request.Context(), md)
+		c.Request = c.Request.WithContext(ctx)
 		c.Next()
 	}
 }
